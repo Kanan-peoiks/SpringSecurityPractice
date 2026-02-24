198aba29994f07646bd33db62f590a742705b711
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-02-24 21:20:29 +0400

    feat: add controller package

---
+++ src/main/java/com/example/ssp/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.example.ssp.controller;
+
+import com.example.ssp.dto.AuthRequestDTO;
+import com.example.ssp.jwt.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    @PostMapping("/login")
+    public String login(@RequestBody AuthRequestDTO request) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+
+        return jwtService.generateToken(request.getUsername());
+    }
+}
