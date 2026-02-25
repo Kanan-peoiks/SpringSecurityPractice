b86452f5a5c4bb2d4b31e69c7064503d4fa80533
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-02-25 17:49:58 +0400

    feat: update controller

--- src/main/java/com/example/ssp/controller/AuthController.java
+++ src/main/java/com/example/ssp/controller/AuthController.java
@@ -1,33 +1,47 @@
 package com.example.ssp.controller;
 
-import com.example.ssp.dto.AuthRequestDTO;
 import com.example.ssp.jwt.JwtService;
+import com.example.ssp.model.User;
+import com.example.ssp.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
 
+    @PostMapping("/register")
+    public String register(@RequestBody User user) {
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole("ROLE_USER");
+
+        userRepository.save(user);
+
+        return "User registered!";
+    }
+
     @PostMapping("/login")
-    public String login(@RequestBody AuthRequestDTO request) {
+    public String login(@RequestBody User request) {
 
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.getUsername(),
-                        request.getPassword()
-                )
-        );
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow();
 
-        return jwtService.generateToken(request.getUsername());
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid password");
+        }
+
+        return jwtService.generateToken(user.getUsername());
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "JWT works successfully!";
     }
 }
